84b042fb97a43060debfee7befdbbfc16fa048f2
Thomas McGookin <thomas.mcgokin17@gmail.com>
2023-12-20 19:44:12 +0000

    added value, depth, left and right and tested value

--- binary_search_trees.py
+++ binary_search_trees.py
@@ -1,1 +1,12 @@
-print("Testing")
+class BinarySearchTree:
+  def __init__(self, value, depth=1):
+    self.value = value
+    self.depth = depth
+    self.left = None
+    self.right = None
+
+# Create a new BST:
+root = BinarySearchTree(15)
+
+# Print root's value below:
+print(root.value)
